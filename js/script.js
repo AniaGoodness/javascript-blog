80e6018d8225c160e4394bc893f9f07806a2bd59
Anna Grabowska <aniagoodness@gmail.com>
2020-02-01 21:52:10 +0100

    add_active

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  this.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post-content p.active');
 
   for(let activeArticle of activeArticles){
