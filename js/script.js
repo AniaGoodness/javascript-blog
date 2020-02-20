9570483a26fd378482a63612df4d48dbcbfb5ebb
Anna Grabowska <aniagoodness@gmail.com>
2020-02-20 21:11:47 +0100

    addActiveToLink

--- js/script.js
+++ js/script.js
@@ -14,34 +14,34 @@ const titleClickHandler = function(event){
   /* [IN PROGRESS] add class 'active' to the clicked link
   dodaj klasę active do klikniętego linka */
   console.log('clickedElement:', clickedElement);
-  for(let clickedElement of clickedElements){
+
   clickedElement.classList.add('active')
-  }
+
   /* [DONE] remove class 'active' from all articles
   usuń klasę active ze wszystkich artykułów */
   const activeArticles = document.querySelectorAll('.post-content p.active');
 
   for(let activeArticle of activeArticles){
-  activeLink.classList.remove('active');
+  activeArticle.classList.remove('active');
   }
 
   /* get 'href' attribute from the clicked link
   z klikniętego linka weź zawartość atrybutu href, np. #article-2,*/
-  const articleSelector = document.clickedElement("href");
-  var a = href.getAttribute("article-1");
+  const articleSelector = clickedElement.getAttribute("href");
 
   /* find the correct article using the selector (value of 'href' attribute)
   znajdź na stronie element pasujący do selektora takiego,
   jak wartość atrybutu href, np. #article-2
   – czyli szukamy elementu o id="article-2",*/
+const questArticle = document.querySelector(articleSelector);
 
 
 
   /* add class 'active' to the correct article
   dodaj klasę active do znalezionego artykułu*/
-  for(let activeArticle of activeArticles){
-  activeLink.classList.add('active');
-  }
+
+  questArticle.classList.add('active');
+
 
 }
 
