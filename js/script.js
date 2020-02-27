c45260439c127cf29da6f5db8ab1b3fd78dd07c1
Anna Grabowska <aniagoodness@gmail.com>
2020-02-27 20:30:31 +0100

    tags

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@
   const optArticleSelector = '.post';
   const optTitleSelector = '.post-title';
   const optTitleListSelector = '.titles';
+  const optArticleTagsSelector = '.post-tags .list';
 
   // eslint-disable-next-line require-jsdoc
   function generateTitleLinks() {
@@ -86,4 +87,33 @@
   for (let link of links) {
     link.addEventListener('click', titleClickHandler);
   }
+  // eslint-disable-next-line require-jsdoc
+  function generateTags() {
+  /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+    /* START LOOP: for every article: */
+    for (let article of articles) {
+    /* find tags wrapper */
+      const titleList = article.querySelector(optArticleTagsSelector);
+      console.log('titleList');
+      /* make html variable with empty string */
+      let html = '';
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log(articleTags);
+      /* split tags into array */
+      const articleTagsArray = articleTags.split(' ');
+      console.log(articleTagsArray);
+    /* START LOOP: for each tag */
+      for (let tag of articleTagsArray){
+        console.log(tag);
+    /* generate HTML of the link */
+    /* add generated code to html variable */
+    }
+  /* END LOOP: for each tag */
+  /* insert HTML of all the links into the tags wrapper */
+  /* END LOOP: for every article: */
+  }
+  generateTags();
+}
 }
